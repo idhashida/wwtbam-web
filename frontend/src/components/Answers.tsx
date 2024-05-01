8ef5f028c9fa449a84e062fbb6368acfa553af3a
idhashida <mb1874@ya.ru>
2024-05-01 19:45:37 +0500

    Компонент вариантов ответа

---
+++ frontend/src/components/Answers.tsx
@@ -0,0 +1,10 @@
+export default function Answers() {
+    return (
+        <div className='grid grid-cols-2 gap-10 h-[50%] w-[50%] m-auto b2d'>
+            <div className="w-full h-full border-2 border-white border-solid rounded-md"></div>
+            <div className="w-full h-full border-2 border-white border-solid rounded-md"></div>
+            <div className="w-full h-full border-2 border-white border-solid rounded-md"></div>
+            <div className="w-full h-full border-2 border-white border-solid rounded-md"></div>
+        </div>
+    )
+}
--- frontend/src/components/Field.tsx
+++ frontend/src/components/Field.tsx
@@ -1,23 +1,24 @@
 import '../index.css'
+import Answers from './Answers'
 import Help from './Help'
 import Logo from './Logo'
 import PrizeTable from './PrizeTable'
 
 export default function Field() {
     return (
-        <div className='p-1 h-dvh bg-primary-default rounded-md'>
-            <div className='flex justify-between p-5 gap-5 b2d'>
+        <div className='flex flex-col p-1 gap-5 h-dvh bg-primary-default rounded-md'>
+            <div className='flex justify-between p-5 gap-32 b2d'>
                 <Help/>
                 <div className='flex flex-col w-full gap-10 b2d'>
+                    <Logo/>
                     <div className="h-full w-full p-5 text-center border-2 border-primary-to50 border-solid rounded-md">
                         <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus cupiditate commodi quos esse? Optio tempora recusandae provident ducimus consectetur veniam.</h1>
                     </div>
-                    <Logo/>
                 </div>
                 <PrizeTable/>
             </div>
-            <div className='flex justify-center h-fit p-5'>
-                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, repellendus.</h1>
+            <div className='flex h-[100%] p-5 b2d'>
+                <Answers/>
             </div>
         </div>
     )
